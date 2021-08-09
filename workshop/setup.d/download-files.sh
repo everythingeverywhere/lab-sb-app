7e25d0278d49c46ea626e44dc34271aab5517b9f
everythingeverywhere <ivan0tarin@gmail.com>
2021-08-09 11:31:09 -0600

    add coras executable

--- workshop/setup.d/download-files.sh
+++ workshop/setup.d/download-files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-mkdir bin && cd bin
+# mkdir bin && cd bin
 
-# Install SDKMAN 
-curl -s "https://get.sdkman.io" | bash
-# make executable
-source "/home/eduk8s/bin/.sdkman/bin/sdkman-init.sh"
+# # Install SDKMAN 
+# curl -s "https://get.sdkman.io" | bash
+# # make executable
+# source "/home/eduk8s/bin/.sdkman/bin/sdkman-init.sh"
 
 export VERSION=2.5.1
 curl -G https://start.spring.io/starter.zip -d dependencies=web -d bootVersion=$VERSION -d artifactId=spring-boot -d name=spring-boot -d groupId=com.example -d language=java -o spring-boot.zip
