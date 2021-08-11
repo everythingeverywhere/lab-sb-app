cbea0deaac35a33bda87d3b19546be6e29f72312
everythingeverywhere <ivan0tarin@gmail.com>
2021-08-11 14:44:12 -0600

    opt2

--- workshop/setup.d/download-files.sh
+++ workshop/setup.d/download-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-mkdir -p /opt && cd bin
+cd opt
 curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.5.3/spring-boot-cli-2.5.3-bin.zip -o /opt/spring.zip
-unzip /opt/spring.zip
+unzip /opt/spring.zip 
 
 export SPRING_HOME=/opt/spring-2.5.3
 export PATH=$SPRING_HOME/bin/spring:$PATH
